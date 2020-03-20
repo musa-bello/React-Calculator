e739d731f83488a5fdd379410f248b2876fd00c5
Musa Bello <mbibrahim.4@gmail.com>
2020-03-20 23:42:19 +0100

    added build folder

---
+++ build/precache-manifest.fbf00bfb899670ab30ab2fec8c387cd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0c0cd1bada410339cf33a4aa9a993e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "793cd9bf29941c5c44be",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "626f6c223b7182e15261",
+    "url": "/static/css/main.3c91f1dc.chunk.css"
+  },
+  {
+    "revision": "793cd9bf29941c5c44be",
+    "url": "/static/js/2.211e3a87.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.211e3a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626f6c223b7182e15261",
+    "url": "/static/js/main.a47ca6f6.chunk.js"
+  },
+  {
+    "revision": "97f5793d088d5204c715",
+    "url": "/static/js/runtime-main.ff80d48d.js"
+  }
+]);
