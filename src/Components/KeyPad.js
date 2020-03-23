bc426fcb7dc43e59b56dab2485ad4e2538667648
Musa Bello <mbibrahim.4@gmail.com>
2020-03-23 20:54:53 +0100

    removed unnecessary files

--- src/Components/KeyPad.js
+++ src/Components/KeyPad.js
@@ -26,34 +26,5 @@ function KeyPad(props) {
             </div>
     )
 }
-/* class KeyPad extends Component{
-    
-    render (){
-        return (
-            <div className="keyboard">
-                <button name="clear" className="btn clear btn-outline-primary" onClick ={this.handleClick}> c </button>
-                <button name="del" className="btn clear btn-outline-primary" onClick ={this.handleClick}>d </button>
-                <button name="sqrt" className="btn clear btn-outline-primary" onClick ={this.handleClick}> &radic; </button>
-                <button name="+" className="btn digit btn-outline-primary" onClick ={this.handleClick}> + </button>
-                <button name="7" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 7 </button>
-                <button name="8" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 8 </button>
-                <button name="9" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 9 </button>
-                <button name="/" className="btn digit btn-outline-primary" onClick ={this.handleClick}> &divide; </button>
-                <button name="4" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 4 </button>
-                <button name="5" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 5 </button>
-                <button name="6" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 6 </button>
-                <button name="-" className="btn digit btn-outline-primary" onClick ={this.handleClick}> &minus; </button>
-                <button name="1" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 1 </button>
-                <button name="2" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 2 </button>
-                <button name="3" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 3 </button>
-                <button name="*" className="btn digit btn-outline-primary" onClick ={this.handleClick}> &times; </button>
-                <button name="." className="btn digit btn-outline-secondary" onClick ={this.handleClick}> &sdot; </button>
-                <button name="0" className="btn digit btn-outline-secondary" onClick ={this.handleClick}> 0 </button>
-                <button name="pi" className="btn clear btn-outline-primary" onClick ={this.handleClick}> &pi; </button>
-                <button name="=" className="btn operator btn-outline-success"  onClick ={this.handleClick}> = </button>
-            </div>
-        )
-    }
-} */
 
 export default KeyPad
